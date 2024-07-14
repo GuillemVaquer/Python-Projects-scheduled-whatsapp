a5ba003b30eac911f49008fdb70f80546e105070
GuillemVaquer <64426112+GuillemVaquer@users.noreply.github.com>
2024-07-14 23:03:55 +0200

    Update main.py (scheduledwhatsapp)

--- SchduledMessageWhatsApp/main.py
+++ SchduledMessageWhatsApp/main.py
@@ -1,19 +1,36 @@
 import pywhatkit
 import datetime
 
-num = input("Enter WhatsApp Number of Sender (without Country Code) : ")
-msg = input("Enter Your Message : ")
-time = input("Enter Time in (HH:MM) 24-hr format : ")
-if len(time) != 5:
-    print("Invalid. Please make sure HH:MM format is followed, without any extra space (include only 5 characters)")
-    exit(1)
-h = int(time[0]+time[1])
-m = int(time[3]+time[4])
-if (h<0 or h>23):
-    print("Invalid")
-    exit(1)
-if (m<0 or m>59):
-    print("Invalid")
-    exit(1)
 
-pywhatkit.sendwhatmsg(f"+91{num}", f"{msg}", h, m)
+
+def main():
+    path = get_path()
+    if path == "c":
+        receiver = input("Enter WhatsApp Number of Sender (without Country Code) : ")
+        msg, sending_time = get_message()
+        pywhatkit.sendwhatmsg(f"+91{receiver}", f"{msg}", sending_time.hour, sending_time.minute)
+    else:
+        receiver = input("Enter the exact name of the Whatsapp group : ")
+        msg, sending_time = get_message()
+        pywhatkit.sendwhatmsg_to_group(f"{receiver}", f"{msg}", sending_time.hour, sending_time.minute)
+    
+
+def get_path():
+    while True:
+        try:
+            path = input("Are you sending a message to a contact or to a group? (c/g): ")
+            if path != ("c" or "g"):
+                raise ValueError
+        except ValueError:
+            print("Please write either 'c' or 'g'.")
+        return path
+
+def get_message():
+    msg = input("Enter Your Message : ")
+    sending_time = input("Enter Time in (HH:MM) 24-hr format : ")
+    sending_time = datetime.time.fromisoformat(sending_time)
+    return msg, sending_time
+
+
+if __name__=="__main__":
+    main()
